d434e2c198ad4e2f0c5ef3c5362165bc720467d1
longluv1605 <22022604@vnu.edu.vn>
2024-10-12 11:10:50 +0700

    done 1

---
+++ Cifar10-scratch/main.py
@@ -0,0 +1,9 @@
+from model import Model
+
+def main():
+    clf = Model()
+    clf.training_and_evaluate()
+    
+
+if __name__ == '__main__':
+    main()
--- Cifar10-scratch/mlp.py
+++ Cifar10-scratch/mlp.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 class MLP(torch.nn.Module):
-    def __init__(self, n_hidden_nodes, n_classes, image_width, image_height, color_channels):
+    def __init__(self, n_hidden_nodes, n_classes, image_width=32, image_height=32, color_channels=3):
         super().__init__()
         self.n_hidden_nodes = n_hidden_nodes
         self.n_classes = n_classes
@@ -15,14 +15,13 @@ class MLP(torch.nn.Module):
         self.color_channels = color_channels
         self.input_size = self.image_width * self.image_height * self.color_channels
         
-        # define layers
         self.hidden_layer = nn.Linear(self.input_size, self.n_hidden_nodes)
         self.output_layer = nn.Linear(self.n_hidden_nodes, self.n_classes)
         
     def forward(self, x):
-        x = x.view(-1, self.image_width, self.input_size)
+        x = x.view(x.size(0), -1)
     
         x = self.relu(self.hidden_layer(x))
-        x = self.sigmoid(self.output_layer())
+        x = self.sigmoid(self.output_layer(x))
         
         return x
