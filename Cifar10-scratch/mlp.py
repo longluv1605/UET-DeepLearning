0789622e9b2e455edfde9eda8b652fd9713baecf
longluv1605 <22022604@vnu.edu.vn>
2024-10-12 18:15:27 +0700

    done 2

--- Cifar10-scratch/mlp.py
+++ Cifar10-scratch/mlp.py
@@ -7,21 +7,24 @@ class MLP(torch.nn.Module):
         self.n_hidden_nodes = n_hidden_nodes
         self.n_classes = n_classes
         
-        self.sigmoid = nn.Sigmoid()
         self.relu = nn.ReLU6()
+        self.leaky_relu = nn.LeakyReLU()
         
         self.image_width = image_width
         self.image_height = image_height
         self.color_channels = color_channels
         self.input_size = self.image_width * self.image_height * self.color_channels
         
-        self.hidden_layer = nn.Linear(self.input_size, self.n_hidden_nodes)
-        self.output_layer = nn.Linear(self.n_hidden_nodes, self.n_classes)
+        # define layers
+        self.hidden_layer = nn.Linear(self.input_size, self.n_hidden_nodes) # (B, c * w * h) -> (B, 100)
+        self.hidden_layer_2 = nn.Linear(self.n_hidden_nodes, self.n_hidden_nodes) # (B, 100) -> (B, 100)
+        self.output_layer = nn.Linear(self.n_hidden_nodes, self.n_classes) # (B, 100) -> (B, nclasses)
         
     def forward(self, x):
         x = x.view(x.size(0), -1)
-    
         x = self.relu(self.hidden_layer(x))
-        x = self.sigmoid(self.output_layer(x))
-        
+        x = self.leaky_relu(self.hidden_layer_2(x))
+        x = self.leaky_relu(self.hidden_layer_2(x))
+        x = self.output_layer(x)
         return x
+
