a5e4aea6970cb1e3a37ab9f8e42cfa3c236c3aac
longluv1605 <22022604@vnu.edu.vn>
2024-11-12 17:39:48 +0700

    mod cifar10 mlp notebook

--- Cifar10-scratch/mlp/main.py
+++ Cifar10-scratch/mlp/main.py
@@ -4,6 +4,10 @@ from hyper_tuning import *
 def main():
     model = MLP(n_classes=10, n_hidden_nodes=100, image_width=32, image_height=32, color_channels=3)
     best_params, best_lost, model = hyper_tuning(model=model, epochs=1, write_log=True, download=False)
+    
+    print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Finished >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    print(f'Best params: {best_params}')
+    print(f'Best loss: {best_lost}')
 
 if __name__ == '__main__':
     main()
