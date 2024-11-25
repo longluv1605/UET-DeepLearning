aa77ffee4c81f51f98e63b8091b3ebf8fa4617a5
longluv1605 <22022604@vnu.edu.vn>
2024-11-26 00:57:15 +0700

    ok

--- Text-Sentiment/models/rnn/rnn_cell.py
+++ Text-Sentiment/models/rnn/rnn_cell.py
@@ -1,20 +1,21 @@
 import torch
 import torch.nn as nn
 
+
 class RNNCell(nn.Module):
-    def __init__(self, input_size, hidden_size, bias=True, nonlinearity='tanh'):
+    def __init__(self, input_size, hidden_size, bias=True, nonlinearity="tanh"):
         super(RNNCell, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.bias = bias
         self.nonlinearity = nonlinearity
-        
-        if self.nonlinearity != 'tanh' and self.nonlinearity != 'relu':
-            raise ValueError('Invalid nonlinearity!')
-        
+
+        if self.nonlinearity != "tanh" and self.nonlinearity != "relu":
+            raise ValueError("Invalid nonlinearity!")
+
         self.x2h = nn.Linear(input_size, hidden_size, bias=bias)
         self.h2h = nn.Linear(hidden_size, hidden_size, bias=bias)
-        
+
     def forward(self, x, hx=None):
         # Input
         #       x: of shape (batch_size, input_size)
@@ -23,19 +24,48 @@ class RNNCell(nn.Module):
         #
         # Output
         #       y: of shape (batch_size, hidden_size)
-        
+
         if hx is None:
             hx = torch.zeros(input.size(0), self.hidden_size)
-            
+
         y = self.x2h(x) + self.h2h(hx)
-        
-        if self.nonlinearity == 'tanh':
+
+        if self.nonlinearity == "tanh":
             y = torch.tanh(y)
         else:
             y = torch.tanh(y)
-            
-        return y
-    
 
-class LSTM_Cell(nn.Module):
-    pass
+        return y
+
+
+class LSTMCell(nn.Module):
+    def __init__(self, input_size, hidden_size, bias):
+        super(LSTMCell, self).__init__()
+        self.bias = bias
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+
+        self.x2h = nn.Linear(input_size, hidden_size)
+        self.h2h = nn.Linear(hidden_size, hidden_size)
+
+    def forward(self, x_t, ht_1=None):
+        if ht_1 is None:
+            ht_1 = torch.zeros(x_t.size(0), self.hidden_size)
+            ht_1 = (ht_1, ht_1)
+
+        ht_1, ct_1 = ht_1
+
+        gates = self.x2h(x_t) + self.h2h(ht_1)
+
+        input_gate, forget_gate, cell_gate, output_gate = gates.chunk(4, 1)
+
+        f_t = torch.sigmoid(forget_gate)
+        i_t = torch.sigmoid(input_gate)
+        c_t_d = torch.tanh(cell_gate)
+        o_t = torch.sigmoid(output_gate)
+
+        c_t = ct_1 * f_t + i_t * c_t_d
+
+        h_t = o_t * torch.tanh(c_t)
+
+        return h_t, c_t
--- Text-Sentiment/models/rnn/rnn_model.py
+++ Text-Sentiment/models/rnn/rnn_model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from rnn_cell import RNNCell
+from rnn_cell import RNNCell, LSTMCell
 
 class SimpleRNN(nn.Module):
     def __init__(self, vocab_size, emb_dim, hidden_size, output_size, emb_matrix=None, dropout=0.5, bias=True, activation='tanh'):
@@ -103,3 +103,62 @@ class StageRNN(SimpleRNN):
         y = self.clf(y)
         
         return y
+    
+
+class LSTM(nn.Module):
+    def __init__(self, vocab_size, emb_dim, hidden_size, output_size, num_layers=1, emb_matrix=None, dropout=0.5, bias=True, activation='tanh'):
+        super(LSTM, self).__init__()
+        
+        self.vocab_size = vocab_size
+        self.emb_dim = emb_dim
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.bias =  bias
+        self.num_layers = num_layers
+        
+        if emb_matrix is None:
+            self.embedding = nn.Embedding(vocab_size, emb_dim)
+        else:
+            self.embedding = nn.Embedding.from_pretrained(emb_matrix, freeze=False)
+            
+        self.lstm_cell_list = nn.ModuleList()
+        
+        self.lstm_cell_list.append(LSTMCell(emb_dim, hidden_size))
+        for _ in range(1, num_layers):
+            self.lstm_cell_list.append(LSTMCell(hidden_size, hidden_size))
+        
+        self.dropout = nn.Dropout(dropout)
+        self.clf = nn.Linear(hidden_size, output_size)
+        
+    
+    def forward(self, x, h0=None):
+        x = self.embedding(x) # of shape (batch_size, seq_length, emb_dim)
+        
+        if h0 is None:
+            if torch.cuda.is_available():
+                h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).cuda()
+            else:
+                h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        
+        
+        outs = []
+        hidden = []
+        for layer in range(self.num_layers):
+            hidden.append((h0[layer, :, :], h0[layer, :, :]))
+        
+        for t in range(x.size(1)):
+            for layer in range(self.num_layers):
+                if layer == 0:
+                    hidden_l = self.lstm_cell_list[layer](x[:, t, :], hidden[layer][0])
+                else:
+                    hidden_l = self.lstm_cell_list[layer](hidden[layer - 1][0], hidden[layer])
+                
+                hidden[layer] = hidden_l
+                
+            outs.append(hidden_l[0])
+        
+        out = outs[-1].squeeze()
+        out = self.clf(out)
+        
+        return out
+                
